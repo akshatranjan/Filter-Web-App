53eecde5a988d113a261126ff4bf2e4997a4d843
akshatranjan <73059364+akshatranjan@users.noreply.github.com>
2021-12-26 11:47:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function preload() {
+noseX = 0;
+noseY = 0;
+
+function preload() 
+{
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    lipstick = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
 }
 
 function setup()
@@ -19,8 +25,10 @@ function getPoses(results)
     if (results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
@@ -32,6 +40,7 @@ function modelLoaded()
 function draw() 
 {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX - 25, noseY + 5, 50, 50);
 }
 
 function take_snapshot()
